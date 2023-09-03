4ee2c03d1cad94ac60e985e5e7166855901d1c7b
sDEV-eloper <thisissurajdev@gmail.com>
2023-09-03 21:06:20 +0530

    working fine

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,26 +1,24 @@
 import React, { useState } from 'react'
 import { POSTER_BASE_URL } from '../utils/constant'
 import VideoTrailerBg from './VideoTrailerBg'
-// import { useSelector } from 'react-redux';
-// import VideoTrailerBg from './VideoTrailerBg';
 
-const MovieCard = ({posterPath, id}) => {
-const [isRendered, setIsRendered]=useState(false)
-  const handleClick=()=>{
-    setIsRendered(!isRendered)
-  }
+const MovieCard = ({posterPath,onClick, isSelected,  id}) => {
     if (!posterPath) return null;
   return (
+    <>
     <div className='flex flex-col gap-4'>
-    <div className="w-36 md:w-48 p-4 hover:scale-110 transition duration-600 cursor-pointer" onClick={handleClick}>
+    <div className="w-36 md:w-48 p-4 hover:scale-110 transition duration-600 cursor-pointer flex flex-col gap-8 " onClick={onClick}>
         <img src={POSTER_BASE_URL+posterPath} alt="" className='rounded-lg border-gray-600'/>
-    </div>
-    {isRendered &&
-     <div className='border flex justify-start '>
+        </div>
+    {isSelected &&
+     <div className='border text-white flex flex-col items-center w-full h-full' >
+     <div className='border left-0'>
      <VideoTrailerBg id={id}/>
      </div>
+     </div>
      }
-    </div>
+     </div>
+     </>
   )
 }
 
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -8,8 +8,8 @@ const VideoTitle = ({title, overview}) => {
       <h1 className='text-4xl font-bold text-white pl-4 '>{title}</h1>
       <p className="p-4  w-2/5 text-gray-200">{overview} </p>
       <div className='flex gap-2 p-4'>
-        <button className=' bg-white border flex py-1 items-center gap-2 rounded px-6 shadow-lg hover:bg-gray-300'><FaPlay/> Play</button>
-        <button className=' border flex gap-2 items-center  rounded py-1 px-6 shadow-lg bg-black bg-opacity-80 text-white hover:bg-gray-800'><AiOutlineInfoCircle/>More Info</button>
+        {/* <button className=' bg-white border flex py-1 items-center gap-2 rounded px-6 shadow-lg hover:bg-gray-300' onClick={}><FaPlay/> Play</button> */}
+        {/* <button className=' border flex gap-2 items-center  rounded py-1 px-6 shadow-lg bg-black bg-opacity-80 text-white hover:bg-gray-800'><AiOutlineInfoCircle/>More Info</button> */}
       </div>
     </div>
   )
